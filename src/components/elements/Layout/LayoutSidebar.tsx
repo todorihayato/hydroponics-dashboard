ae8b7b287db5a5f49802c65107abf71765d47200
Ayuu <ayuu.kumakuma@gmail.com>
2023-02-07 16:56:27 +0900

    🐛 パスが/hogeの状態でリロードした時にタブだけデフォルトの位置に戻ってしまうバグを修正

--- src/components/elements/Layout/LayoutSidebar.tsx
+++ src/components/elements/Layout/LayoutSidebar.tsx
@@ -1,23 +1,47 @@
 import { Box, Divider, TabList, Tabs, Text } from '@chakra-ui/react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { LayoutTab } from './LayoutTab'
+
+type PathName = '' | 'temperature' | 'water_temperature' | 'co2' | 'humidity' | 'ec' | 'control'
 
 export const LayoutSidebar = () => {
   const navigate = useNavigate()
+  const currentPath = useLocation().pathname
+  const revertPath = (currentPath: PathName) => {
+    const pathName = currentPath.slice(1)
+    switch (pathName) {
+      case '':
+        return 0
+      case 'temperature':
+        return 1
+      case 'water_temperature':
+        return 2
+      case 'co2':
+        return 3
+      case 'humidity':
+        return 4
+      case 'ec':
+        return 5
+      case 'control':
+        return 6
+      default:
+        return 0
+    }
+  }
   const convertPath = (index: number) => {
     switch (index) {
       case 0:
         return ''
       case 1:
-        return 'ec'
-      case 2:
-        return 'co2'
-      case 3:
         return 'temperature'
-      case 4:
+      case 2:
         return 'water_temperature'
-      case 5:
+      case 3:
+        return 'co2'
+      case 4:
         return 'humidity'
+      case 5:
+        return 'ec'
       case 6:
         return 'control'
       default:
@@ -40,6 +64,7 @@ export const LayoutSidebar = () => {
       </Box>
       <Box mt={'32px'} w={'100%'} display={'flex'} justifyContent={'center'}>
         <Tabs
+          defaultIndex={revertPath(currentPath as PathName)}
           orientation={'vertical'}
           variant={'unstyled'}
           onChange={(index) => navigate(`/${convertPath(index)}`)}
@@ -47,13 +72,13 @@ export const LayoutSidebar = () => {
           <TabList>
             <LayoutTab>Dashboard</LayoutTab>
             <Divider opacity={1}/>
-            <LayoutTab>EC</LayoutTab>
+            <LayoutTab>Temperature</LayoutTab>
+            <LayoutTab>Water Temp</LayoutTab>
             <LayoutTab>
               CO<sub>2</sub>
             </LayoutTab>
-            <LayoutTab>Temperature</LayoutTab>
-            <LayoutTab>Water Temp</LayoutTab>
             <LayoutTab>Humidity</LayoutTab>
+            <LayoutTab>EC</LayoutTab>
             <Divider opacity={1}/>
             <LayoutTab>Control</LayoutTab>
           </TabList>
